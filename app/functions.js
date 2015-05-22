4d022600220b8c9c4b6a6fa120155bb3f5381785
J2D2Development <joedriscoll79@gmail.com>
2015-05-22 16:25:38 -0700

    More practice in functions section

--- app/functions.js
+++ app/functions.js
@@ -16,8 +16,9 @@ define(function() {
         }
     },
 
+    //this works, but one of the tests is to make sure the result from funcs === result from square on each array element.  trouble with version below is that it just returns the result from each function, not the function iteslf- find a way to get it to return a
     makeClosures : function(arr, fn) {
-        return arr.forEach(fn(n));
+        return arr.map(fn);
     },
 
     partial : function(fn, str1, str2) {
@@ -39,8 +40,15 @@ define(function() {
 
     },
 
+    //this is a terrible answer, as it depends on knowing how many arguments are being passed to the original fn.  tried to use array slice method to turn arguments into array and pass to fn, but couldn't figure out how to get the arguments off the passed in function (fn)
     curryIt : function(fn) {
-
+        return function(x) {
+            return function(y) {
+                return function(z) {
+                    return fn(x,y,z);
+                }
+            }
+        }
     }
   };
 });
